0474b7227b64338d089314178c9f0ca9a8c175dc
jamesjavanyc` <1931736857@qq.com>
2023-11-01 16:45:45 -0400

    Add project files.

---
+++ Migrations/20231101200425_Rating2.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MvcMovie.Migrations
+{
+    /// <inheritdoc />
+    public partial class Rating2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Image",
+                table: "Movie",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Image",
+                table: "Movie",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+        }
+    }
+}
---
+++ Models/SeedData.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models;
+
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new MvcMovieContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<MvcMovieContext>>()))
+        {
+            // Look for any movies.
+            if (context.Movie.Any())
+            {
+                return;   // DB has been seeded
+            }
+            context.Movie.AddRange(
+                new Movie
+                {
+                    Title = "The RM",
+                    ReleaseDate = DateTime.Parse("1989-2-12"),
+                    Genre = "Church",
+                    Price = 7.99M,
+                    Rating = "R",
+                    Image = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/A-1.jpg/1024px-A-1.jpg"
+                },
+                new Movie
+                {
+                    Title = "Other Side of Heaven ",
+                    ReleaseDate = DateTime.Parse("1984-3-13"),
+                    Genre = "Church1",
+                    Price = 8.99M,
+                    Rating = "R",
+                    Image = "https://commons.wikimedia.org/wiki/File:A-1J_6th_SOS_over_Vietnam_c1968.jpg"
+                },
+                new Movie
+                {
+                    Title = "Meet the Mormons",
+                    ReleaseDate = DateTime.Parse("1986-2-23"),
+                    Genre = "Church",
+                    Price = 9.99M,
+                    Rating = "R",
+                    Image = "https://upload.wikimedia.org/wikipedia/en/d/db/View_-1.jpg"
+                },
+                new Movie
+                {
+                    Title = "Meet the Mormons",
+                    ReleaseDate = DateTime.Parse("1959-4-15"),
+                    Genre = "Church",
+                    Price = 3.99M,
+                    Rating = "R",
+                    Image = "https://it.m.wikipedia.org/wiki/File:Torre_del_gallo.JPG"
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
